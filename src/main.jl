328425f2799d2c08333f42a28cf0dc522a7b3bd3
Pieter <p.vermeesch@ucl.ac.uk>
2024-11-08 12:13:51 +0000

    Checks if extension is already present

--- src/main.jl
+++ src/main.jl
@@ -1,12 +1,13 @@
 function extend!(_PT::AbstractDict)
 
-    message = "i. Internal isochron\n" * _PT["tree"]["extra"].message
-    action = _PT["tree"]["extra"].action
-    action["i"] = TUIintchron
-    
-    updateTree!(_PT["tree"],"extra",
-                message = message,
-                action = action)
+    if !haskey(_PT["tree"]["extra"].action,"i")
+        message = "i. Internal isochron\n" * _PT["tree"]["extra"].message
+        action = _PT["tree"]["extra"].action
+        action["i"] = TUIinternochron
+        updateTree!(_PT["tree"],"extra",
+                    message = message,
+                    action = action)
+    end
 
 end
 export extend!
@@ -19,13 +20,13 @@ function updateTree!(tree::AbstractDict,
     tree[key] = (message=message,help=help,action=action)
 end
 
-function TUIintchron(ctrl::AbstractDict)
-    P, D, d = atomic(ctrl["run"][ctrl["i"]],
-                     ctrl["channels"],
-                     ctrl["blank"],
-                     ctrl["par"])
+function TUIinternochron(ctrl::AbstractDict)
+    P, D, d = Plasmatrace.atomic(ctrl["run"][ctrl["i"]],
+                                 ctrl["channels"],
+                                 ctrl["blank"],
+                                 ctrl["par"])
     x0, y0 = internochron(P,D,d)
     Plots.plot([0,x0],[y0,0])
     Plots.plot!(P/D,d/D, seriestype=:scatter)
 end
-export TUIintchron
+export TUIinternochron
